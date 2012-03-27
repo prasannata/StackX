08ccf770699ba6fe6e6bff5747ddde22c5714519
Prasanna Thirumalai <prasanna.ta@gmail.com>
2012-03-27 12:12:39 -0700

    For child layout, replace FILL_PARENT with MATCH_PARENT

--- src/com/prasanna/android/stacknetwork/UserHomeActivity.java
+++ src/com/prasanna/android/stacknetwork/UserHomeActivity.java
@@ -34,22 +34,22 @@ public class UserHomeActivity extends AbstractUserActionBarActivity
 
     static class GzipDecompressingEntity extends HttpEntityWrapper
     {
-	public GzipDecompressingEntity(final HttpEntity entity)
-	{
-	    super(entity);
-	}
+        public GzipDecompressingEntity(final HttpEntity entity)
+        {
+            super(entity);
+        }
 
-	@Override
-	public InputStream getContent() throws IOException, IllegalStateException
-	{
-	    return new GZIPInputStream(wrappedEntity.getContent());
-	}
+        @Override
+        public InputStream getContent() throws IOException, IllegalStateException
+        {
+            return new GZIPInputStream(wrappedEntity.getContent());
+        }
 
-	@Override
-	public long getContentLength()
-	{
-	    return -1;
-	}
+        @Override
+        public long getContentLength()
+        {
+            return -1;
+        }
     }
 
     private Intent questionsIntent;
@@ -67,142 +67,137 @@ public class UserHomeActivity extends AbstractUserActionBarActivity
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
-	super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);
 
-	setContentView(R.layout.questions_layout);
+        setContentView(R.layout.questions_layout);
 
-	masterLinearLayout = (LinearLayout) findViewById(R.id.homeDisplay);
-	scrollView = (ScrollViewWithNotifier) masterLinearLayout.findViewById(R.id.questionsScroll);
-	scrollView.setUsingActivity(this);
-	questionsLinearLayout = (LinearLayout) scrollView.findViewById(R.id.questionsDisplay);
-	// setSiteHeader();
+        masterLinearLayout = (LinearLayout) findViewById(R.id.homeDisplay);
+        scrollView = (ScrollViewWithNotifier) masterLinearLayout.findViewById(R.id.questionsScroll);
+        scrollView.setUsingActivity(this);
+        questionsLinearLayout = (LinearLayout) scrollView.findViewById(R.id.questionsDisplay);
+        // setSiteHeader();
 
-	startReceiverAndService();
+        startReceiverAndService();
     }
 
     private void startReceiverAndService()
     {
-	progressDialog = ProgressDialog
-	        .show(UserHomeActivity.this, "", getString(R.string.loading));
+        progressDialog = ProgressDialog.show(UserHomeActivity.this, "", getString(R.string.loading));
 
-	registerQuestionsReceiver();
+        registerQuestionsReceiver();
 
-	startQuestionsService();
+        startQuestionsService();
     }
 
     private void startQuestionsService()
     {
-	questionsIntent = new Intent(this, UserQuestionsIntentService.class);
-	questionsIntent.setAction(IntentActionEnum.QuestionIntentAction.ALL_QUESTIONS.name());
-	questionsIntent.putExtra("page", ++page);
-	startService(questionsIntent);
-	serviceRunning = Boolean.TRUE;
+        questionsIntent = new Intent(this, UserQuestionsIntentService.class);
+        questionsIntent.setAction(IntentActionEnum.QuestionIntentAction.ALL_QUESTIONS.name());
+        questionsIntent.putExtra("page", ++page);
+        startService(questionsIntent);
+        serviceRunning = Boolean.TRUE;
     }
 
     private void registerQuestionsReceiver()
     {
-	IntentFilter filter = new IntentFilter(
-	        IntentActionEnum.QuestionIntentAction.ALL_QUESTIONS.name());
-	filter.addCategory(Intent.CATEGORY_DEFAULT);
-	registerReceiver(getReceiver(), filter);
+        IntentFilter filter = new IntentFilter(IntentActionEnum.QuestionIntentAction.ALL_QUESTIONS.name());
+        filter.addCategory(Intent.CATEGORY_DEFAULT);
+        registerReceiver(getReceiver(), filter);
     }
 
     @Override
     protected void onDestroy()
     {
-	super.onDestroy();
-	stopServiceAndUnregisterReceiver();
+        super.onDestroy();
+        stopServiceAndUnregisterReceiver();
     }
 
     private void stopServiceAndUnregisterReceiver()
     {
-	if (questionsIntent != null)
-	{
-	    stopService(questionsIntent);
-	}
+        if (questionsIntent != null)
+        {
+            stopService(questionsIntent);
+        }
 
-	try
-	{
-	    unregisterReceiver(getReceiver());
-	}
-	catch (IllegalArgumentException e)
-	{
-	    Log.d(TAG, e.getMessage());
-	}
+        try
+        {
+            unregisterReceiver(getReceiver());
+        }
+        catch (IllegalArgumentException e)
+        {
+            Log.d(TAG, e.getMessage());
+        }
     }
 
     @Override
     public void onStop()
     {
-	super.onStop();
+        super.onStop();
 
-	stopServiceAndUnregisterReceiver();
+        stopServiceAndUnregisterReceiver();
     }
 
     private void processQuestions(ArrayList<Question> questions)
     {
-	if (progressDialog != null)
-	{
-	    progressDialog.dismiss();
-	    progressDialog = null;
-	}
+        if (progressDialog != null)
+        {
+            progressDialog.dismiss();
+            progressDialog = null;
+        }
 
-	if (loadingProgressView != null)
-	{
-	    questionsLinearLayout.removeView(loadingProgressView);
-	    loadingProgressView.setVisibility(View.GONE);
-	    loadingProgressView = null;
-	}
+        if (loadingProgressView != null)
+        {
+            questionsLinearLayout.removeView(loadingProgressView);
+            loadingProgressView.setVisibility(View.GONE);
+            loadingProgressView = null;
+        }
 
-	for (final Question question : questions)
-	{
-	    LinearLayout topLayout = LayoutBuilder.getInstance().buildQuestionSnippet(this,
-		    question);
-	    questionsLinearLayout.addView(topLayout, new LinearLayout.LayoutParams(
-		    LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
-	}
+        for (final Question question : questions)
+        {
+            LinearLayout topLayout = LayoutBuilder.getInstance().buildQuestionSnippet(this, question);
+            questionsLinearLayout.addView(topLayout, new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT,
+                    LayoutParams.WRAP_CONTENT));
+        }
 
-	serviceRunning = Boolean.FALSE;
+        serviceRunning = Boolean.FALSE;
     }
 
     @Override
     public void scrollViewToBottomNotifier()
     {
-	Log.d(TAG, "Scroll view to bottom action invoked");
+        Log.d(TAG, "Scroll view to bottom action invoked");
 
-	if (serviceRunning.equals(Boolean.FALSE))
-	{
-	    loadingProgressView = (LinearLayout) getLayoutInflater().inflate(
-		    R.layout.loading_progress, null);
-	    LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
-		    LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT);
-	    layoutParams.setMargins(0, 15, 0, 15);
-	    questionsLinearLayout.addView(loadingProgressView, layoutParams);
-	    startQuestionsService();
-	}
+        if (serviceRunning.equals(Boolean.FALSE))
+        {
+            loadingProgressView = (LinearLayout) getLayoutInflater().inflate(R.layout.loading_progress, null);
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT,
+                    LayoutParams.WRAP_CONTENT);
+            layoutParams.setMargins(0, 15, 0, 15);
+            questionsLinearLayout.addView(loadingProgressView, layoutParams);
+            startQuestionsService();
+        }
     }
 
     @Override
     public void refresh()
     {
-	stopServiceAndUnregisterReceiver();
-	questionsLinearLayout.removeAllViews();
-	startReceiverAndService();
+        stopServiceAndUnregisterReceiver();
+        questionsLinearLayout.removeAllViews();
+        startReceiverAndService();
     }
 
     @Override
     public View getActiveParentView()
     {
-	return questionsLinearLayout;
+        return questionsLinearLayout;
     }
 
     @SuppressWarnings("unchecked")
     @Override
     public void processReceiverIntent(Context context, Intent intent)
     {
-	ArrayList<Question> questions = (ArrayList<Question>) intent
-	        .getSerializableExtra(IntentActionEnum.QuestionIntentAction.ALL_QUESTIONS
-	                .getExtra());
-	processQuestions(questions);
+        ArrayList<Question> questions = (ArrayList<Question>) intent
+                .getSerializableExtra(IntentActionEnum.QuestionIntentAction.ALL_QUESTIONS.getExtra());
+        processQuestions(questions);
     }
 }
