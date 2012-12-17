fae3be6de300d633f8d205956fe4303c147d38a0
Prasanna Thirumalai <prasanna.ta@gmail.com>
2012-12-16 16:45:31 -0800

    Model for error

---
+++ src/com/prasanna/android/stacknetwork/model/StackXError.java
@@ -0,0 +1,17 @@
+package com.prasanna.android.stacknetwork.model;
+
+import java.io.Serializable;
+
+public class StackXError implements Serializable
+{
+    private static final long serialVersionUID = -8292786331139417559L;
+
+    public int statusCode;
+    
+    public int id;
+    
+    public String name;
+    
+    public String msg;
+
+}
