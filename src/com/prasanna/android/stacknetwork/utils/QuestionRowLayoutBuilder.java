7ee07c229742be7639324884bce65b3f0909b860
Prasanna Thirumalai <prasanna.ta@gmail.com>
2013-01-09 10:52:20 -0800

    Create a new row for tags when no space available
    When tags are displayed for a question and no space is available to fit
    them in a row, wrap tags by creating a new row

--- src/com/prasanna/android/stacknetwork/utils/QuestionRowLayoutBuilder.java
+++ src/com/prasanna/android/stacknetwork/utils/QuestionRowLayoutBuilder.java
@@ -21,6 +21,8 @@ package com.prasanna.android.stacknetwork.utils;
 
 import android.content.Context;
 import android.text.Html;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
@@ -31,6 +33,8 @@ import com.prasanna.android.stacknetwork.model.Question;
 
 public class QuestionRowLayoutBuilder
 {
+    private static final String TAG = QuestionRowLayoutBuilder.class.getSimpleName();
+
     private static QuestionRowLayoutBuilder builder = new QuestionRowLayoutBuilder();
 
     private QuestionRowLayoutBuilder()
@@ -39,61 +43,92 @@ public class QuestionRowLayoutBuilder
 
     public static QuestionRowLayoutBuilder getInstance()
     {
-	return builder;
+        return builder;
     }
 
     public LinearLayout build(final LayoutInflater layoutInflater, final Context context, final Question question)
     {
-	LinearLayout questionRowLayout = (LinearLayout) layoutInflater.inflate(R.layout.question_snippet_layout, null);
+        LinearLayout questionRowLayout = (LinearLayout) layoutInflater.inflate(R.layout.question_snippet_layout, null);
 
-	questionRowLayout.setId((int) question.id);
-	setupViewForQuestionMetadata(context, questionRowLayout, question);
-	setupViewForTags(questionRowLayout, context, question);
-	return questionRowLayout;
+        questionRowLayout.setId((int) question.id);
+        setupViewForQuestionMetadata(context, questionRowLayout, question);
+        setupViewForTags(questionRowLayout, context, question);
+        return questionRowLayout;
     }
 
     private void setupViewForTags(final LinearLayout parentLayout, final Context context, final Question question)
     {
-	LinearLayout tagsParentLayout = (LinearLayout) parentLayout.findViewById(R.id.questionSnippetTags);
+        LinearLayout tagsParentLayout = (LinearLayout) parentLayout.findViewById(R.id.questionSnippetTags);
+        DisplayMetrics display = context.getResources().getDisplayMetrics();
+        int maxWidth = display.widthPixels - 10;
 
-	if (question.tags != null && question.tags.length > 0)
-	{
-	    for (int i = 0; i < question.tags.length; i++)
-	    {
-		LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-		                LinearLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-		params.setMargins(3, 0, 3, 0);
-		TextView tagTextView = ((TextView) getInflater(context).inflate(R.layout.tags_layout, null));
-		tagTextView.setText(question.tags[i]);
-		tagsParentLayout.addView(tagTextView, params);
-	    }
-	}
+        Log.d(TAG, "Max width for tag row: " + maxWidth);
+
+        LinearLayout rowLayout = createNewRowForTags(context, 0);
+
+        if (question.tags != null && question.tags.length > 0)
+        {
+            for (int i = 0; i < question.tags.length; i++)
+            {
+                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                                LinearLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+                params.setMargins(3, 0, 3, 0);
+                TextView tagTextView = ((TextView) getInflater(context).inflate(R.layout.tags_layout, null));
+                tagTextView.setText(question.tags[i]);
+
+                tagTextView.measure(LinearLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+                rowLayout.measure(LinearLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+                Log.d(TAG, "Current textview width: " + rowLayout.getMeasuredWidth());
+
+                if ((tagTextView.getMeasuredWidth() + rowLayout.getMeasuredWidth()) > maxWidth)
+                {
+                    tagsParentLayout.addView(rowLayout);
+                    rowLayout = createNewRowForTags(context, 3);
+                }
+
+                rowLayout.addView(tagTextView, params);
+            }
+
+            tagsParentLayout.addView(rowLayout);
+        }
+    }
+
+    private LinearLayout createNewRowForTags(final Context context, int topMargin)
+    {
+        LinearLayout rowLayout = new LinearLayout(context);
+        rowLayout.setOrientation(LinearLayout.HORIZONTAL);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT);
+        layoutParams.topMargin = topMargin;
+        rowLayout.setLayoutParams(layoutParams);
+        return rowLayout;
     }
 
     private void setupViewForQuestionMetadata(Context context, LinearLayout parentLayout, Question question)
     {
-	TextView textView = (TextView) parentLayout.findViewById(R.id.score);
-	textView.setText(AppUtils.formatNumber(question.score));
+        TextView textView = (TextView) parentLayout.findViewById(R.id.score);
+        textView.setText(AppUtils.formatNumber(question.score));
 
-	textView = (TextView) parentLayout.findViewById(R.id.answerCount);
-	textView.setText(AppUtils.formatNumber(question.answerCount));
+        textView = (TextView) parentLayout.findViewById(R.id.answerCount);
+        textView.setText(AppUtils.formatNumber(question.answerCount));
 
-	if (question.hasAcceptedAnswer)
-	    textView.setBackgroundColor(context.getResources().getColor(R.color.lichen));
+        if (question.hasAcceptedAnswer)
+            textView.setBackgroundColor(context.getResources().getColor(R.color.lichen));
 
-	textView = (TextView) parentLayout.findViewById(R.id.itemTitle);
-	textView.setText(Html.fromHtml(question.title));
+        textView = (TextView) parentLayout.findViewById(R.id.itemTitle);
+        textView.setText(Html.fromHtml(question.title));
 
-	textView = (TextView) parentLayout.findViewById(R.id.questionViewsValue);
-	textView.append(":" + AppUtils.formatNumber(question.viewCount));
+        textView = (TextView) parentLayout.findViewById(R.id.questionViewsValue);
+        textView.append(":" + AppUtils.formatNumber(question.viewCount));
 
-	textView = (TextView) parentLayout.findViewById(R.id.questionOwner);
-	textView.setText(DateTimeUtils.getElapsedDurationSince(question.creationDate) + " by "
-	                + Html.fromHtml(question.owner.displayName));
+        textView = (TextView) parentLayout.findViewById(R.id.questionOwner);
+        textView.setText(DateTimeUtils.getElapsedDurationSince(question.creationDate) + " by "
+                        + Html.fromHtml(question.owner.displayName));
     }
 
     private LayoutInflater getInflater(Context context)
     {
-	return (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        return (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 }
