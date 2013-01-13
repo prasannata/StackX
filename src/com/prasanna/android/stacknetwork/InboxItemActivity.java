7bbac161275a8d6b4f0c418b37c535e2ddaee356
Prasanna Thirumalai <prasanna.ta@gmail.com>
2013-01-13 10:34:21 -0800

    Rework inbox item detail layout.

--- src/com/prasanna/android/stacknetwork/InboxItemActivity.java
+++ src/com/prasanna/android/stacknetwork/InboxItemActivity.java
@@ -24,9 +24,13 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.text.Html;
 import android.util.Log;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -50,6 +54,7 @@ public class InboxItemActivity extends AbstractUserActionBarActivity implements 
     private RestQueryResultReceiver receiver;
     private InboxItem item;
     private Comment comment;
+    private ContextMenu menu;
 
     @Override
     public void onCreate(android.os.Bundle savedInstanceState)
@@ -101,26 +106,11 @@ public class InboxItemActivity extends AbstractUserActionBarActivity implements 
 
         getActionBar().setTitle(Html.fromHtml(item.title));
 
-        TextView textView = (TextView) findViewById(R.id.itemTitle);
+        TextView textView = (TextView) findViewById(R.id.postTitle);
         textView.setText(Html.fromHtml(item.title));
 
-        if (item.body != null)
-        {
-            textView = (TextView) findViewById(R.id.itemBodyPreview);
-            textView.setVisibility(View.GONE);
-        }
-
-        textView = (TextView) findViewById(R.id.itemCreationTime);
-        textView.setText(DateTimeUtils.getElapsedDurationSince(item.creationDate));
-
-        textView = (TextView) findViewById(R.id.itemType);
+        textView = (TextView) findViewById(R.id.postType);
         textView.setText(item.itemType.getRepr());
-
-        if (item.site != null)
-        {
-            textView = (TextView) findViewById(R.id.itemSite);
-            textView.setText(item.site.name);
-        }
     }
 
     @Override
@@ -131,6 +121,56 @@ public class InboxItemActivity extends AbstractUserActionBarActivity implements 
         menu.removeItem(R.id.menu_refresh);
 
         return ret & true;
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo)
+    {
+        Log.d(TAG, "onCreateContextMenu");
+
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        getMenuInflater().inflate(R.menu.question_context_menu, menu);
+
+        menu.removeItem(R.id.q_ctx_comments);
+        menu.removeItem(R.id.q_ctx_menu_archive);
+        menu.removeItem(R.id.q_ctx_menu_email);
+        menu.removeItem(R.id.q_ctx_menu_tags);
+        menu.removeItem(R.id.q_ctx_related);
+
+        this.menu = menu;
+
+        setupUserProfileInContextMenu();
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.q_ctx_menu_user_profile:
+                viewUserProfile();
+                return true;
+
+        }
+
+        return false;
+    }
+
+    private void viewUserProfile()
+    {
+        long userId = post != null ? post.owner.id : comment.owner.id;
+        Intent userProfileIntent = new Intent(this, UserProfileActivity.class);
+        userProfileIntent.putExtra(StringConstants.USER_ID, userId);
+        startActivity(userProfileIntent);
+    }
+
+    private void setupUserProfileInContextMenu()
+    {
+        MenuItem userProfileMenuItem = menu.findItem(R.id.q_ctx_menu_user_profile);
+        String userName = post == null ? comment.owner.displayName : post.owner.displayName;
+        if (userProfileMenuItem != null && userName != null)
+            userProfileMenuItem.setTitle(userName + "'s profile");
     }
 
     @Override
@@ -161,9 +201,7 @@ public class InboxItemActivity extends AbstractUserActionBarActivity implements 
             comment = (Comment) resultData.getSerializable(StringConstants.COMMENT);
             if (comment != null)
                 showPostDetail(comment);
-
         }
-
     }
 
     private void showPostDetail(StackXItem stackXItem)
@@ -173,14 +211,52 @@ public class InboxItemActivity extends AbstractUserActionBarActivity implements 
         setProgressBarIndeterminateVisibility(false);
 
         TextView textView = (TextView) findViewById(R.id.responseUserAndTime);
-        textView.setText(DateTimeUtils.getElapsedDurationSince(stackXItem.creationDate) + " ago by "
+        textView.setText(DateTimeUtils.getElapsedDurationSince(stackXItem.creationDate) + " by "
                         + Html.fromHtml(stackXItem.owner.displayName));
+
+        textView = (TextView) findViewById(R.id.postSite);
+        textView.setText("Asked in " + item.site.name);
+
+        setupOnClickForViewQuestion();
 
         textView = (TextView) findViewById(R.id.responseItemScore);
         textView.setText(String.valueOf(stackXItem.score));
 
+        setuOnClickForContextMenu();
+
         LinearLayout postBodyLayout = (LinearLayout) findViewById(R.id.postBody);
         for (View view : MarkdownFormatter.parse(getApplicationContext(), stackXItem.body))
             postBodyLayout.addView(view);
+    }
+
+    private void setuOnClickForContextMenu()
+    {
+        ImageView imageView = (ImageView) findViewById(R.id.responseContextMenu);
+        registerForContextMenu(imageView);
+        imageView.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                InboxItemActivity.this.openContextMenu(v);
+            }
+        });
+    }
+
+    private void setupOnClickForViewQuestion()
+    {
+        TextView textView;
+        textView = (TextView) findViewById(R.id.viewQuestion);
+        textView.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent displayQuestionIntent = new Intent(InboxItemActivity.this, QuestionActivity.class);
+                displayQuestionIntent.setAction(StringConstants.QUESTION_ID);
+                displayQuestionIntent.putExtra(StringConstants.QUESTION_ID, item.questionId);
+                startActivity(displayQuestionIntent);
+            }
+        });
     }
 }
