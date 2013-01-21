c140edfd4e29e7159529fd8985e27e305484eac0
Prasanna Thirumalai <prasanna.ta@gmail.com>
2013-01-21 10:32:40 -0800

    Clear stack after selecting a site.

--- src/com/prasanna/android/stacknetwork/adapter/SiteListAdapter.java
+++ src/com/prasanna/android/stacknetwork/adapter/SiteListAdapter.java
@@ -51,100 +51,101 @@ public class SiteListAdapter extends AbstractDraggableArrayListAdpater<Site>
 
     public SiteListAdapter(Context context, int textViewResourceId, List<Site> sites, ListView listView)
     {
-	super(context, textViewResourceId, sites, listView);
+        super(context, textViewResourceId, sites, listView);
 
-	layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
     @Override
     public View getView(final int position, View convertView, ViewGroup parent)
     {
-	RelativeLayout layoutForSites = (RelativeLayout) convertView;
-	if (dataSet != null && position >= 0 && position < dataSet.size())
-	{
-	    layoutForSites = (RelativeLayout) layoutInflater.inflate(R.layout.sitelist_row, null);
-	    TextView textView = (TextView) layoutForSites.findViewById(R.id.siteName);
-	    textView.setGravity(Gravity.LEFT);
-	    textView.setId(dataSet.get(position).name.hashCode());
-	    textView.setText(dataSet.get(position).name);
+        RelativeLayout layoutForSites = (RelativeLayout) convertView;
+        if (dataSet != null && position >= 0 && position < dataSet.size())
+        {
+            layoutForSites = (RelativeLayout) layoutInflater.inflate(R.layout.sitelist_row, null);
+            TextView textView = (TextView) layoutForSites.findViewById(R.id.siteName);
+            textView.setGravity(Gravity.LEFT);
+            textView.setId(dataSet.get(position).name.hashCode());
+            textView.setText(dataSet.get(position).name);
 
-	    if (dataSet.get(position).userType.equals(UserType.REGISTERED))
-	    {
-		textView = (TextView) layoutForSites.findViewById(R.id.siteUserTypeRegistered);
-		textView.setVisibility(View.VISIBLE);
-	    }
+            if (dataSet.get(position).userType.equals(UserType.REGISTERED))
+            {
+                textView = (TextView) layoutForSites.findViewById(R.id.siteUserTypeRegistered);
+                textView.setVisibility(View.VISIBLE);
+            }
 
-	    setViewAndListenerForDefaultSiteOption(position, layoutForSites);
-	    setOnClickForSite(position, layoutForSites);
-	}
+            setViewAndListenerForDefaultSiteOption(position, layoutForSites);
+            setOnClickForSite(position, layoutForSites);
+        }
 
-	return layoutForSites;
+        return layoutForSites;
     }
 
     private void setViewAndListenerForDefaultSiteOption(final int position, RelativeLayout layoutForSites)
     {
-	String currentDefaultSite = SharedPreferencesUtil.getDefaultSiteName(getContext());
-	final ImageView iv = (ImageView) layoutForSites.findViewById(R.id.isDefaultSite);
-	if (isDefaultSite(currentDefaultSite, position))
-	    iv.setImageResource(R.drawable.circle_delft);
-	iv.setOnClickListener(new View.OnClickListener()
-	{
-	    @Override
-	    public void onClick(View v)
-	    {
-		String currentDefaultSite = SharedPreferencesUtil.getDefaultSiteName(getContext());
-		if (isDefaultSite(currentDefaultSite, position))
-		{
-		    iv.setImageResource(R.drawable.circle_white);
-		    SharedPreferencesUtil.clearDefaultSite(getContext());
-		}
-		else
-		{
-		    iv.setImageResource(R.drawable.circle_delft);
-		    SharedPreferencesUtil.setDefaultSiteName(getContext(), dataSet.get(position).name);
-		    SharedPreferencesUtil.setDefaultSite(getContext(), dataSet.get(position));
-		    notifyDataSetChanged();
-		    Toast.makeText(getContext(),
-			            dataSet.get(position).name
-			                            + " set as default site. You can change it in settings anytime.",
-			            Toast.LENGTH_LONG).show();
-		}
-	    }
-	});
+        String currentDefaultSite = SharedPreferencesUtil.getDefaultSiteName(getContext());
+        final ImageView iv = (ImageView) layoutForSites.findViewById(R.id.isDefaultSite);
+        if (isDefaultSite(currentDefaultSite, position))
+            iv.setImageResource(R.drawable.circle_delft);
+        iv.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                String currentDefaultSite = SharedPreferencesUtil.getDefaultSiteName(getContext());
+                if (isDefaultSite(currentDefaultSite, position))
+                {
+                    iv.setImageResource(R.drawable.circle_white);
+                    SharedPreferencesUtil.clearDefaultSite(getContext());
+                }
+                else
+                {
+                    iv.setImageResource(R.drawable.circle_delft);
+                    SharedPreferencesUtil.setDefaultSiteName(getContext(), dataSet.get(position).name);
+                    SharedPreferencesUtil.setDefaultSite(getContext(), dataSet.get(position));
+                    notifyDataSetChanged();
+                    Toast.makeText(getContext(), dataSet.get(position).name + " set as default site.",
+                                    Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
     private boolean isDefaultSite(String currentDefaultSite, final int position)
     {
-	return currentDefaultSite != null && currentDefaultSite.equals(dataSet.get(position).name);
+        return currentDefaultSite != null && currentDefaultSite.equals(dataSet.get(position).name);
     }
 
     private void setOnClickForSite(final int position, RelativeLayout layoutForSites)
     {
-	layoutForSites.setOnClickListener(new View.OnClickListener()
-	{
-	    public void onClick(View v)
-	    {
-		if (reorder == false)
-		{
-		    Log.d(TAG, "Clicking on list item " + position);
+        layoutForSites.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                if (reorder == false)
+                {
+                    Log.d(TAG, "Clicking on list item " + position);
 
-		    Site site = dataSet.get(position);
-		    OperatingSite.setSite(site);
-		    Intent startQuestionActivityIntent = new Intent(listView.getContext(), QuestionsActivity.class);
-		    listView.getContext().startActivity(startQuestionActivityIntent);
-		}
-	    }
-	});
+                    Site site = dataSet.get(position);
+                    OperatingSite.setSite(site);
+                    Intent startQuestionActivityIntent = new Intent(listView.getContext(), QuestionsActivity.class);
+                    startQuestionActivityIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startQuestionActivityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    listView.getContext().startActivity(startQuestionActivityIntent);
+                    Toast.makeText(getContext(), "Use options menu to change site any time.", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
     public List<Site> getSites()
     {
-	return dataSet;
+        return dataSet;
     }
 
     @Override
     public String getTag()
     {
-	return TAG;
+        return TAG;
     }
 }
