ac94224894c456582a70b08dffbafb5f281d74a0
Prasanna Thirumalai <prasanna.ta@gmail.com>
2013-01-21 18:14:03 -0800

    Show accept rate only when available.

--- src/com/prasanna/android/stacknetwork/fragment/UserProfileFragment.java
+++ src/com/prasanna/android/stacknetwork/fragment/UserProfileFragment.java
@@ -203,8 +203,12 @@ public class UserProfileFragment extends Fragment implements StackXRestQueryResu
         textView = (TextView) profileHomeLayout.findViewById(R.id.profileViews);
         textView.setText(getString(R.string.views) + " " + user.profileViews);
 
-        textView = (TextView) profileHomeLayout.findViewById(R.id.profileAcceptRate);
-        textView.setText(getString(R.string.acceptRate) + " " + user.acceptRate + "%");
+        if (user.acceptRate > -1)
+        {
+            textView = (TextView) profileHomeLayout.findViewById(R.id.profileAcceptRate);
+            textView.setText(getString(R.string.acceptRate) + " " + user.acceptRate + "%");
+            textView.setVisibility(View.VISIBLE);
+        }
 
         textView = (TextView) profileHomeLayout.findViewById(R.id.profileUserLastSeen);
         textView.setText(getString(R.string.lastSeen) + " "
