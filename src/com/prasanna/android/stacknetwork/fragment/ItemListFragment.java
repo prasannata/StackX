fd74d8df88511d9690172e520672ffbe658765ac
Prasanna Thirumalai <prasanna.ta@gmail.com>
2013-01-31 17:02:06 -0800

    Remove unused import

--- src/com/prasanna/android/stacknetwork/fragment/ItemListFragment.java
+++ src/com/prasanna/android/stacknetwork/fragment/ItemListFragment.java
@@ -27,7 +27,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
