10700305994b8ca9763105952cce5eca197d4ba0
Prasanna Thirumalai <prasanna.ta@gmail.com>
2013-12-27 22:54:39 -0800

    Fix compilation of tests.

--- stackx-tests/src/test/java/com/prasanna/android/stacknetwork/service/AbstractBaseServiceHelperTest.java
+++ stackx-tests/src/test/java/com/prasanna/android/stacknetwork/service/AbstractBaseServiceHelperTest.java
@@ -77,7 +77,7 @@ public abstract class AbstractBaseServiceHelperTest {
 
   protected List<BasicNameValuePair> getBasicNameValuePartListForWriteComment() {
     List<BasicNameValuePair> parameters = new ArrayList<BasicNameValuePair>();
-    parameters.add(new BasicNameValuePair(StackUri.QueryParams.ACCESS_TOKEN, AppUtils.getAccessToken(null)));
+    parameters.add(new BasicNameValuePair(StackUri.QueryParams.ACCESS_TOKEN, AppUtils.loadAccessToken(null)));
     parameters.add(new BasicNameValuePair(StackUri.QueryParams.KEY, StackUri.QueryParamDefaultValues.KEY));
     parameters.add(new BasicNameValuePair(StackUri.QueryParams.CLIENT_ID, QueryParamDefaultValues.CLIENT_ID));
     parameters.add(new BasicNameValuePair(StackUri.QueryParams.SITE, OperatingSite.getSite().apiSiteParameter));
